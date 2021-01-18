f85b14cfb21d55162d942acef1db6da82179fa18
Oriol Sanz <oriol.sanz.vericat.sa@everis.com>
2021-01-18 14:10:33 +0100

    ADD: create project

---
+++ oriol-sanz-vericat-ios/oriol-sanz-vericat-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oriol-sanz-vericat-ios
+//
+//  Created by Oriol Sanz Vericat on 18/01/2021.
+//  Copyright © 2021 Oriol Sanz Vericat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ oriol-sanz-vericat-ios/oriol-sanz-vericat-iosTests/oriol_sanz_vericat_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  oriol_sanz_vericat_iosTests.swift
+//  oriol-sanz-vericat-iosTests
+//
+//  Created by Oriol Sanz Vericat on 18/01/2021.
+//  Copyright © 2021 Oriol Sanz Vericat. All rights reserved.
+//
+
+import XCTest
+@testable import oriol_sanz_vericat_ios
+
+class oriol_sanz_vericat_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
